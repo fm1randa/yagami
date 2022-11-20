936e4eb64f527b4c68af5e9d4141c3a41f5916a3
Filipe Miranda <fmiranda@tempo.io>
2022-11-20 18:22:32 -0300

    feat(connectToDatabase): add error handling and Mongo Store return

--- src/app/database.ts
+++ src/app/database.ts
@@ -2,15 +2,26 @@ import mongoose from "mongoose";
 import dotenv, { DotenvConfigOptions } from "dotenv";
 import { format, FormatInputPathObject } from "path";
 import { logger } from "../helpers";
+import { MongoStore } from "wwebjs-mongo";
 
 export async function connectToDatabase(mongoURI: string) {
-	const path: FormatInputPathObject = {
-		base: ".env",
-	};
-	const options: DotenvConfigOptions = {
-		path: format(path),
-	};
-	dotenv.config(options);
-	logger.info("Connecting to MongoDB...");
-	return mongoose.connect(mongoURI);
+	try {
+		const path: FormatInputPathObject = {
+			base: ".env",
+		};
+		const options: DotenvConfigOptions = {
+			path: format(path),
+		};
+		dotenv.config(options);
+		logger.info("Connecting to MongoDB...");
+		await mongoose.connect(mongoURI);
+		logger.info("Connected to MongoDB");
+		const store = new MongoStore({ mongoose });
+		return {
+			mongoose,
+			store,
+		};
+	} catch (error) {
+		logger.error(`An error occurred while connecting to MongoDB: ${error}`);
+	}
 }
