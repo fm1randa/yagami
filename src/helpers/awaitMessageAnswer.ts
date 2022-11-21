b2aef74463cbfbb0739635df6dea7d5228417755
Filipe Miranda <fmiranda@tempo.io>
2022-11-20 21:12:57 -0300

    refactor(awaitMessageAnswer): translate to english

--- src/helpers/awaitMessageAnswer.ts
+++ src/helpers/awaitMessageAnswer.ts
@@ -1,87 +1,81 @@
 import { Client, Message } from "whatsapp-web.js";
 
 export type AwaitAnswerOptionsType = {
-    requireQuote?: boolean;
-    fromAnyUserInChat?: boolean;
+  requireQuote?: boolean;
+  fromAnyUserInChat?: boolean;
 };
 
 type ValidateMessagesArgs = {
-    options: AwaitAnswerOptionsType;
-    afterMessage: Message;
-    watchMessage: Message;
+  options: AwaitAnswerOptionsType;
+  afterMessage: Message;
+  watchMessage: Message;
 };
 
 function awaitAnswer(
-    client: Client,
-    watchMessage: Message,
-    options?: AwaitAnswerOptionsType
+  client: Client,
+  watchMessage: Message,
+  options?: AwaitAnswerOptionsType
 ) {
-    return new Promise<Message>((resolve, reject) => {
-        const listener = async (afterMessage: Message) => {
-            try {
-                const fromSameChat = await isFromSameChat(
-                    afterMessage,
-                    watchMessage
-                );
-                if (!fromSameChat) {
-                    return;
-                }
-                if (
-                    !options?.fromAnyUserInChat &&
-                    !isFromSameUser(afterMessage, watchMessage)
-                ) {
-                    return;
-                }
-                const isValid = await validateMessages({
-                    options,
-                    afterMessage,
-                    watchMessage,
-                });
-                if (!isValid) {
-                    reject(
-                        "A mensagem não foi respondida. O comando foi cancelado."
-                    );
-                }
-                resolve(afterMessage);
+  return new Promise<Message>((resolve, reject) => {
+    const listener = async (afterMessage: Message) => {
+      try {
+        const fromSameChat = await isFromSameChat(afterMessage, watchMessage);
+        if (!fromSameChat) {
+          return;
+        }
+        if (
+          !options?.fromAnyUserInChat &&
+          !isFromSameUser(afterMessage, watchMessage)
+        ) {
+          return;
+        }
+        const isValid = await validateMessages({
+          options,
+          afterMessage,
+          watchMessage,
+        });
+        if (!isValid) {
+          reject("Message wasn't answered. Command cancelled");
+        }
+        resolve(afterMessage);
 
-                client.removeListener("message_create", listener);
-            } catch (error) {
-                reject(error);
-            }
-        };
-        client.on("message_create", listener);
-    });
+        client.removeListener("message_create", listener);
+      } catch (error) {
+        reject(error);
+      }
+    };
+    client.on("message_create", listener);
+  });
 }
 async function validateMessages(args: ValidateMessagesArgs) {
-    const { options, afterMessage, watchMessage } = args;
+  const { options, afterMessage, watchMessage } = args;
 
-    if (!options?.requireQuote) {
-        return true;
-    }
-
-    const quoted = await afterMessage.getQuotedMessage();
-    const quotedSameMessage = isSameMessage(quoted, watchMessage);
-
-    if (!quotedSameMessage) {
-        return false;
-    }
+  if (!options?.requireQuote) {
     return true;
+  }
+
+  const quoted = await afterMessage.getQuotedMessage();
+  const quotedSameMessage = isSameMessage(quoted, watchMessage);
+
+  if (!quotedSameMessage) {
+    return false;
+  }
+  return true;
 }
 
 async function isFromSameChat(message: Message, watchMessage: Message) {
-    const messageChat = await message.getChat();
-    const watchMessageChat = await watchMessage.getChat();
-    const sameId =
-        messageChat.id._serialized === watchMessageChat.id._serialized;
-    return sameId;
+  const messageChat = await message.getChat();
+  const watchMessageChat = await watchMessage.getChat();
+  const sameId = messageChat.id._serialized === watchMessageChat.id._serialized;
+  return sameId;
 }
 
 function isFromSameUser(message: Message, watchMessage: Message) {
-    return message.from === watchMessage.from;
+  return message.from === watchMessage.from;
 }
 
 function isSameMessage(message: Message, watchMessage: Message) {
-    return message.id.id === watchMessage.id.id;
+  return message.id.id === watchMessage.id.id;
 }
 
 export default awaitAnswer;
