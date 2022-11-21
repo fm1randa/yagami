5c7b2ef76ccc68728ed23920022d3e7cf4986b11
Filipe Miranda <fmiranda@tempo.io>
2022-11-20 21:10:30 -0300

    refactor: remove vip functionality

--- src/app/collections/User.ts
+++ src/app/collections/User.ts
@@ -3,109 +3,85 @@ import { logger } from "../../helpers";
 import User from "../models/User";
 
 const UserSchema = new mongoose.Schema({
-    contactId: Object,
-    name: String,
-    totalCommandsCalled: Number,
-    lastCommandExecuted: Date,
-    isAdmin: Boolean,
-    isVIP: Boolean,
-    banned: Boolean,
-    commandBlocked: Boolean,
-    isMyContact: Boolean,
+  contactId: Object,
+  name: String,
+  totalCommandsCalled: Number,
+  lastCommandExecuted: Date,
+  isAdmin: Boolean,
+  banned: Boolean,
+  isMyContact: Boolean,
 });
 
 const UserModel = mongoose.model("users", UserSchema);
 
 class UserCollection {
-    async getAll() {
-        const users: User[] = (await UserModel.find().exec()) as User[];
-        return users;
-    }
+  async getAll() {
+    const users: User[] = (await UserModel.find().exec()) as User[];
+    return users;
+  }
 
-    async getAdmins() {
-        const admins: User[] = (await this.find({ isAdmin: true })) as User[];
-        return admins;
-    }
+  async getAdmins() {
+    const admins: User[] = (await this.find({ isAdmin: true })) as User[];
+    return admins;
+  }
 
-    async getVIPs() {
-        const vips: User[] = (await this.find({ isVIP: true })) as User[];
-        return vips;
-    }
+  async addAdmin(_serialized: string) {
+    return this.update(_serialized, { isAdmin: true });
+  }
 
-    async addAdmin(_serialized: string) {
-        return this.update(_serialized, { isAdmin: true });
-    }
+  async removeAdmin(_serialized: string) {
+    return this.update(_serialized, { isAdmin: false });
+  }
 
-    async addVIP(_serialized: string) {
-        return this.update(_serialized, { isVIP: true });
+  async increaseTotalCommandsCalled(_serialized: string) {
+    try {
+      await this.update(_serialized, {
+        $inc: { totalCommandsCalled: 1 },
+        $set: { lastCommandExecuted: new Date() },
+      });
+      const user = await this.getById(_serialized);
+      if (!user) return;
+    } catch (error) {
+      logger.error(
+        "Erro ao incrementar o número de comandos executados do usuário",
+        error
+      );
     }
+  }
 
-    async removeAdmin(_serialized: string) {
-        return this.update(_serialized, { isAdmin: false });
-    }
+  async find(query: object) {
+    return UserModel.find(query).exec();
+  }
 
-    async removeVIP(_serialized: string) {
-        return this.update(_serialized, { isVIP: false });
-    }
+  async getByName(name: string) {
+    const user: User[] = (await UserModel.find({
+      name,
+    }).exec()) as User[];
+    return user;
+  }
+  async getById(_serialized: string) {
+    const user: User = (await UserModel.findOne({
+      "contactId._serialized": _serialized,
+    }).exec()) as User;
+    return user;
+  }
 
-    async increaseTotalCommandsCalled(_serialized: string) {
-        try {
-            await this.update(_serialized, {
-                $inc: { totalCommandsCalled: 1 },
-                $set: { lastCommandExecuted: new Date() },
-            });
-            const user = await this.getById(_serialized);
-            if (!user) return;
-            if (
-                user.totalCommandsCalled % 10 === 0 &&
-                !user.isVIP &&
-                !user.isAdmin
-            ) {
-                await this.update(_serialized, {
-                    commandBlocked: true,
-                });
-            }
-        } catch (error) {
-            logger.error(
-                "Erro ao incrementar o número de comandos executados do usuário",
-                error
-            );
-        }
-    }
+  async create(user: User) {
+    const createdUser = new UserModel(user);
+    await createdUser.save();
+    return createdUser;
+  }
 
-    async find(query: object) {
-        return UserModel.find(query).exec();
-    }
+  async update(_serialized: string, update: object) {
+    return UserModel.findOneAndUpdate(
+      { "contactId._serialized": _serialized },
+      update
+    );
+  }
 
-    async getByName(name: string) {
-        const user: User[] = (await UserModel.find({
-            name,
-        }).exec()) as User[];
-        return user;
-    }
-    async getById(_serialized: string) {
-        const user: User = (await UserModel.findOne({
-            "contactId._serialized": _serialized,
-        }).exec()) as User;
-        return user;
-    }
-
-    async create(user: User) {
-        const createdUser = new UserModel(user);
-        await createdUser.save();
-        return createdUser;
-    }
-
-    async update(_serialized: string, update: object) {
-        return UserModel.findOneAndUpdate(
-            { "contactId._serialized": _serialized },
-            update
-        );
-    }
-
-    async delete(_serialized: string) {
-        return UserModel.deleteOne({ contactId: { _serialized } }).exec();
-    }
+  async delete(_serialized: string) {
+    return UserModel.deleteOne({ contactId: { _serialized } }).exec();
+  }
 }
 
 export default new UserCollection();
--- src/app/models/Group.ts
+++ src/app/models/Group.ts
@@ -3,36 +3,34 @@ import { ChatId } from "whatsapp-web.js";
 import GroupCollection from "../collections/Group";
 
 type GroupPrimaryAttributes = {
-	_id?: mongoose.Types.ObjectId;
-	contactId: ChatId;
-	name: string;
+  _id?: mongoose.Types.ObjectId;
+  contactId: ChatId;
+  name: string;
 };
 export default class Group {
-	public _id?: mongoose.Types.ObjectId;
-	public contactId: ChatId;
-	public name: string;
-	public totalCommandsCalled: number;
-	public lastCommandExecuted: Date | null;
-	public isVIP: boolean;
-	public banned: boolean;
-	public lastCleanup: Date;
+  public _id?: mongoose.Types.ObjectId;
+  public contactId: ChatId;
+  public name: string;
+  public totalCommandsCalled: number;
+  public lastCommandExecuted: Date | null;
+  public banned: boolean;
+  public lastCleanup: Date;
 
-	constructor(attributes: GroupPrimaryAttributes) {
-		if (attributes._id) this._id = attributes._id;
-		this.contactId = attributes.contactId;
-		this.name = attributes.name;
-		this.totalCommandsCalled = 0;
-		this.lastCommandExecuted = null;
-		this.isVIP = false;
-		this.banned = false;
-		this.lastCleanup = null;
-	}
+  constructor(attributes: GroupPrimaryAttributes) {
+    if (attributes._id) this._id = attributes._id;
+    this.contactId = attributes.contactId;
+    this.name = attributes.name;
+    this.totalCommandsCalled = 0;
+    this.lastCommandExecuted = null;
+    this.banned = false;
+    this.lastCleanup = null;
+  }
 
-	async save() {
-		const find = await GroupCollection.getById(this.contactId._serialized);
-		if (find) {
-			return GroupCollection.update(this.contactId._serialized, this);
-		}
-		return GroupCollection.create(this);
-	}
+  async save() {
+    const find = await GroupCollection.getById(this.contactId._serialized);
+    if (find) {
+      return GroupCollection.update(this.contactId._serialized, this);
+    }
+    return GroupCollection.create(this);
+  }
 }
